624f913eaa6d04e4abc4de1fbbebf537b88f7f65
Your Name <you@example.com>
2023-08-03 01:34:07 +0200

    error middleware

--- server/main.py
+++ server/main.py
@@ -23,8 +23,26 @@ async def background_tasks(app):
     app["rev_server"] = asyncio.create_task(shared.rev.start(HOST, REV_PORT))
     yield
 
+def json_error(code, e):
+    return web.json_response({"error": e.__class__.__name__, "detail": str(e)}, status=code)
+
+async def error_middleware(app, handler):
+    async def m(request):
+        try:
+            response = await handler(request)
+            if response.status == 404: return json_error(404, Exception(response.message))
+            return response
+        except web.HTTPException as e:
+            if e.status == 404: return json_error(404, e)
+            raise
+        except Exception as e:
+            print(f"Request {request} has failed with exception: {repr(e)}")
+            return json_error(500, e)
+
+    return m
+
 def main():
-    app = web.Application()
+    app = web.Application(middlewares=[error_middleware])
     aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("./templates"))
     app.cleanup_ctx.append(background_tasks)
     app.add_routes(routes)
