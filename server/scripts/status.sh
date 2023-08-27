e935941cc51eb52f6c8fe16a354954e87752df84
Your Name <you@example.com>
2023-08-27 04:45:28 +0200

    posix

--- server/scripts/status.sh
+++ server/scripts/status.sh
@@ -6,5 +6,5 @@ printf mem_free=; grep MemFree /proc/meminfo | sed 's/.*:\s*//'
 printf uptime=; uptime
 printf pci=; cut -f2 /proc/bus/pci/devices | tr '\n' ',' | sed 's/.$/\n/'
 printf modules=; echo `ls /.__EL_SNEEDIO__/modules.d`
-printf cpu_usage=; awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1) "%"; }' <(touch /.__EL_SNEEDIO__/stat | grep 'cpu ') <(sleep 1; touch /.__EL_SNEEDIO__/stat | grep 'cpu ')
+printf cpu_usage=; (touch /.__EL_SNEEDIO__/stat | grep 'cpu ' && sleep 1 && touch /.__EL_SNEEDIO__/stat | grep 'cpu ') | awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1) "%"; }'
 echo END
