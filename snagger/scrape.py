03448d661e27bfaca91f636a6b4be6d11210ccfb
Your Name <you@example.com>
2023-08-27 05:51:07 +0200

    typo

--- snagger/scrape.py
+++ snagger/scrape.py
@@ -1,4 +1,4 @@
 import requests
 
 def get_vncs(query='text LIKE "%tty%" OR text LIKE "%linux%" OR text LIKE "%ubuntu%" ORDER BY services.updated_at DESC', amount=25, offset=0):
-    return requests.get("https://jew.69.mu/api/search", params={"query": query, "amount": amount, "offset": offset}).json()
+    return requests.get("https://jew.69.mu/api/search", params={"query": query, "amt": amount, "offset": offset}).json()
